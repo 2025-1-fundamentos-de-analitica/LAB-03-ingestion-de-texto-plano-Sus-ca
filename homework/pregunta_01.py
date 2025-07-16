49e574d3e0d138e651d8cfc12afab5bd8bbe778f
Sus-ca <scuenca@unal.edu.co>
2025-07-15 23:01:29 -0500

    intento 1

--- homework/pregunta_01.py
+++ homework/pregunta_01.py
@@ -15,6 +15,49 @@ def pregunta_01():
       espacios por guiones bajos.
     - Las palabras clave deben estar separadas por coma y con un solo
       espacio entre palabra y palabra.
-
-
     """
+    import pandas as pd
+
+    filas = []
+    fila_actual = []
+    with open("files/input/clusters_report.txt", encoding="utf-8") as archivo:
+        lineas = archivo.readlines()
+
+    for linea in lineas[4:]:
+        if linea.strip():
+            fila_actual.append(linea.strip())
+        else:
+            if fila_actual:
+                filas.append(" ".join(fila_actual))
+                fila_actual = []
+    
+    if fila_actual:
+        filas.append(" ".join(fila_actual))
+
+    datos = []
+    for fila in filas:
+        partes = fila.split()
+        id_cluster = int(partes[0])
+        total_palabras = int(partes[1])
+        porcentaje_palabras = float(partes[2].replace(",", "."))
+        palabras_clave = (
+            " ".join(partes[3:])
+            .replace(" ,", ",")
+            .replace(", ", ", ")
+            .strip("%")
+            .rstrip(".")
+            .strip()
+        )
+        datos.append([id_cluster, total_palabras, porcentaje_palabras, palabras_clave])
+
+    columnas = [
+        "cluster",
+        "cantidad_de_palabras_clave",
+        "porcentaje_de_palabras_clave",
+        "principales_palabras_clave"
+    ]
+    df = pd.DataFrame(datos, columns=columnas)
+    print(df)
+    return df
+
+pregunta_01()
